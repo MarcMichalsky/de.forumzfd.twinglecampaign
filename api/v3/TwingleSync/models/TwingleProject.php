b633c8d8871b99d5395d2429ea767839f3b0fa01
Marc Michalsky forumZFD <michalsky@forumzfd.de>
2020-09-28 14:14:59 +0200

    add setters & getters for TwingleProject class

--- api/v3/TwingleSync/models/TwingleProject.php
+++ api/v3/TwingleSync/models/TwingleProject.php
@@ -138,5 +138,49 @@ class TwingleProject {
 
   }
 
+  /**
+   * @return mixed
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * @param mixed $id
+   */
+  public function setId($id): void {
+    $this->id = $id;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getProjectId() {
+    return $this->project_id;
+  }
+
+  /**
+   * @param mixed $project_id
+   */
+  public function setProjectId($project_id): void {
+    $this->project_id = $project_id;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getTimestamp() {
+    return $this->timestamp;
+  }
+
+  /**
+   * @param mixed $timestamp
+   */
+  public function setTimestamp($timestamp): void {
+    $this->timestamp = $timestamp;
+  }
+
+
+
 
 }
